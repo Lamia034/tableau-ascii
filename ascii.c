8058d4c4a975022fc701fda134029cf7037fe883
Lamia BOURICH <lamiascience034@gmail.com>
2022-09-08 09:53:29 +0100

    tableau ascii

---
+++ ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int i;
+	for(i>='A';i<='Z';i++){
+		printf("la lettre: %c  code ascii: %d  code hexa: %x \n",i,i,i);
+	}
+	for(i>='1';i<='9';i++){
+		printf("le chiffre: %c  code ascii: %d  code hexa: %x \n",i,i,i);
+	}
+}
